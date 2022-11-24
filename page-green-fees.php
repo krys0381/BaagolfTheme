c71dfc5ef2856c9ed6ff5ab5592a892b27bd8e52
krys0381 <krys0381@easv365.dk>
2022-11-25 00:15:56 +0100

    Working on Green Fees subpage - 1

--- page-green-fees.php
+++ page-green-fees.php
@@ -18,4 +18,81 @@
   </div>
 </div>
 
+<!-- GREEN FEES - PAY AND PLAY 9-HOLE PAR 3 -->
+
+<div class="payandplay9par3-component container-fluid">
+  
+  <div class="payandplay9par3-row g-0 container">
+    <div class="row d-flex justify-content-between align-items-start">
+      <div class="col-lg-12 col-sm-12 col-12 d-flex flex-column align-items-center">
+        <div class="payandplay9par3-container d-flex flex-column justify-content-between">
+          <div class="payandplay9par3-header-text-container d-flex flex-column">
+            <h2>Pay and Play 9-hole Par 3 Course</h2>
+            <p>Available to All Guests</p>
+          </div>
+          <div class="payandplay9par3-text-container d-flex flex-column align-items-center">
+            <p>
+              The 9-hole Pay & Play par 3 course located on the meadow area with Ho Bugt. Naturally, everyone can also play here.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="cards-row g-0 container">
+    <div class="row d-flex justify-content-between align-items-start">
+      <div class="col-lg-12 col-sm-12 col-12 d-flex flex-column align-items-center">
+
+      <div class="row row-cols-1 row-cols-md-3 g-4 d-flex justify-content-center" style="width: 80%;">
+        <div class="col-lg-3 col-sm-12 col-12">
+          <div class="card">
+            <h3 class="card-title">
+              100 DKK
+              <p>Senior PP3</p>
+            </h3>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Ticket Description:</li>
+              <li class="list-group-item">Above 15 yrs old</li>
+              <li class="list-group-item">9-hole par 3 course</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-12 col-12">
+          <div class="card">
+            <h3 class="card-title">
+              50 DKK
+              <p>Junior PP3</p>
+            </h3>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Ticket Description: </li>
+              <li class="list-group-item">15 yrs old and below</li>
+              <li class="list-group-item">9-hole par 3 course</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-12 col-12">
+          <div class="card">
+            <h3 class="card-title">
+              2 500 DKK
+              <p>Group Beginner Package</p>
+            </h3>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Ticket Description:</li>
+              <li class="list-group-item">Group of 15 people</li>
+              <li class="list-group-item">9-hole par 3 course</li>
+              <li class="list-group-item">Training</li>
+              <li class="list-group-item">Golf equipment</li>
+              <li class="list-group-item">Tee balls</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      </div>
+    </div>
+  </div>
+
+</div>
+
 <?php get_footer() ?>
