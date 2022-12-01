61168848f43f129e96d568fcfdfded9eddf0037d
krys0381 <krys0381@easv365.dk>
2022-12-01 20:55:34 +0100

    Making adjustments 1

--- header.php
+++ header.php
@@ -17,17 +17,18 @@
                 <div class="header-row row d-none d-xl-flex">
                     <div class="desktop-menu-container d-none d-xl-flex justify-content-between">
 
-                        <div class="left-menu-container d-flex col-6 align-items-center justify-content-start" >
-                            <div class="logo-container col-5">
+                        <div class="left-menu-container d-flex col-4 align-items-center justify-content-start" >
+                            <div class="logo-container col-8">
                                 <div class="image-container d-flex justify-content-start align-items-center">
                                     <?php the_custom_logo() ?>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="right-menu-container col-6 d-flex align-items-center justify-content-end g-2">
+                        <div class="right-menu-container col-8 d-flex align-items-center justify-content-end g-2">
                             <div class="header-navigation d-flex g-2">
                                 <?php wp_nav_menu(array("theme_location" => "heared-nav-menu-location")) ?>
+                                <a href="#"><button type="button" class=" btn btn-primary btn-lg col-lg-12 col-sm-12 col-12">BECOME A MEMBER</button></a>
                             </div>
                         </div> 
 
