d16d65a46ffa3b428ebe781e97ceefbd8f224927
krys0381 <krys0381@easv365.dk>
2022-12-02 19:20:41 +0100

    Adding Contact subpage and Robots.txt

---
+++ blocks/lazyblock-contact-form-and-map-block/block.php
@@ -0,0 +1,34 @@
+<!-- CONTACT - CONTACT FORM & MAP -->
+
+<div class="contact-container container-fluid overflow-hidden">
+  <div class="container">
+    <div class="row d-flex justify-content-between">
+      <div class="contact-form-container d-flex justify-content-center flex-column col-lg-5 col-sm-12 col-12">
+        <div class="contact-form-content-container">
+            <form action="" class="form col-lg-6 col-sm-12 col-12 align-items-end" ref="form">
+                <div class="mb-3 d-flex flex-column align-items-start">
+                    <label class="form-label"><?php echo $attributes["form-label-1"]?></label>
+                    <input type="text" class="form-control" id="nameInput" placeholder="" required>
+                </div>
+                <div class="mb-3 d-flex flex-column align-items-start">
+                    <label class="form-label"><?php echo $attributes["form-label-2"]?></label>
+                    <input type="email" class="form-control" id="emailInput" placeholder="" required>
+                </div>
+                <div class="mb-3 d-flex flex-column align-items-start">
+                    <label class="form-label"><?php echo $attributes["form-label-3"]?></label>
+                    <input type="telNumber" class="form-control" id="telNumberInput" placeholder="" required>
+                </div>
+                <div class="mb-3 d-flex flex-column align-items-start">
+                    <label class="form-label"><?php echo $attributes["form-label-4"]?></label>
+                    <textarea type="text" class="form-control" id="messageInput" placeholder="" required></textarea>
+                </div>
+                <a href="<?php echo $attributes["button-link"]?>"><button type="submit" class="btn btn-primary col-12 col-xl-12 btn-lg mt-3" name="send">SEND MESSAGE</button></a>
+            </form>
+        </div>
+      </div>
+      <div class="contact-image col-lg-6 col-sm-12 col-12 d-flex align-items-center">
+        <iframe src="<?php echo $attributes["embedded-map"]?>" width="600" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ blocks/lazyblock-contact-hero-block/block.php
@@ -0,0 +1,17 @@
+<!-- CONTACT - HERO  -->
+
+<div class="contact-hero-component container-fluid d-flex align-items-center justify-content-center overflow-hidden">  
+  <div class="container content-container d-flex align-items-center justify-content-center">
+    <div class="row d-flex flex-column align-items-center justify-content-start">
+      <div class="contact-hero-content-container col-lg-10 col-sm-12 col-12 d-flex flex-column flex-wrap align-items-center justify-content-center">
+        <h1><?php echo $attributes["title"]?></h1>
+        <p class="col-lg-8 my-3">
+            <?php echo $attributes["description"]?>
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="bg-image-container d-none d-xl-flex align-item-center justify-self-center">
+    <img src="<?php echo $attributes["background-image"]?>" alt="">
+  </div>
+</div>
