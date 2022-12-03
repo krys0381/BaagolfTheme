0ff6c4ee9c438ff4e1240018c0d5b7f1a4c705f6
krys0381 <krys0381@easv365.dk>
2022-12-03 13:16:41 +0100

    Fixing Checkout and Order Confirmation

--- page-order-confirmation.php
+++ page-order-confirmation.php
@@ -7,6 +7,7 @@
                 <div class="order-container d-flex flex-column justify-content-between">
                     <?php if(have_posts()): ?>
                         <?php while(have_posts()): the_post() ?>
+                            <h2><?php the_title() ?></h2>
                             <?php the_content() ?>
                         <?php endwhile; ?>
                     <?php else: ?>
